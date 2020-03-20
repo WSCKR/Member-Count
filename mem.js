1e77c183105bc625b139aed840d09a06dc6b257c
WSCKR <60631058+WSCKR@users.noreply.github.com>
2020-03-20 16:00:59 -0400

    updated javascript code

--- mem.js
+++ mem.js
@@ -6,27 +6,27 @@ const token = require('./config.json').token;
 client.login(token);
 
 client.on('ready', () => {
-    let Guild = client.guilds.cache.get('688372475314307106');
+    let Guild = client.guilds.cache.get('ID');
     let memberCount = Guild.memberCount;
-    let memberCountChannel = Guild.channels.cache.get('690397921601912864');
+    let memberCountChannel = Guild.channels.cache.get('ID');
     memberCountChannel.setName('Members: ' + memberCount)
     .then(result => console.log(result))
     .catch(error => console.log(error));
 });
 
 client.on('guildMemberAdd', () => {
-    let Guild = client.guilds.cache.get('688372475314307106');
+    let Guild = client.guilds.cache.get('ID');
     let memberCount = Guild.memberCount;
-    let memberCountChannel = Guild.channels.cache.get('690397921601912864');
+    let memberCountChannel = Guild.channels.cache.get('ID');
     memberCountChannel.setName('Members: ' + memberCount)
     .then(result => console.log(result))
     .catch(error => console.log(error));
 });
 
 client.on('guildMemberRemove', () => {
-    let Guild = client.guilds.cache.get('688372475314307106');
+    let Guild = client.guilds.cache.get('ID');
     let memberCount = Guild.memberCount;
-    let memberCountChannel = Guild.channels.cache.get('690397921601912864');
+    let memberCountChannel = Guild.channels.cache.get('ID');
     memberCountChannel.setName('Members: ' + memberCount)
     .then(result => console.log(result))
     .catch(error => console.log(error));
